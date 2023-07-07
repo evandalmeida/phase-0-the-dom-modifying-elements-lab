a2c225bbff1fdb49d70f6d3b9d259901b0ea9ea4
evandalmeida <edalmei1@gmail.com>
2023-07-07 07:19:59 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Evan is the champion';
